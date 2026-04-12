c5adac252f09375860e15d80a1d7f203ccdebc2c
souhila <souhila.azzouz@monboreal.ca>
2026-04-12 13:41:26 -0400

    Test de modification d'une voiture

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ v1 = Voiture("Toyota", "Corolla", 2020, 20000)
 v1.afficher_voiture()
 
 
-from crud_db import connecter_db, supprimer_voiture, recuperer_voitures
+from crud_db import connecter_db, supprimer_voiture, recuperer_voitures, modifier_voiture
 
 conn = connecter_db()
 print("Connexion réussie")
@@ -16,6 +16,10 @@ from voiture import Voiture
 v1 = Voiture("Honda", "Civic", 2021, 22000)
 ajouter_voiture(v1)
 voitures = recuperer_voitures()
+
 for v in voitures:
     print(v)
 supprimer_voiture(1)
+v = Voiture("Ford", "Focus", 2022, 25000, 2)
+modifier_voiture(v)
+
