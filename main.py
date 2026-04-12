bfd2187854baed90665c3c225c28cd824f9f1da8
souhila <souhila.azzouz@monboreal.ca>
2026-04-12 13:22:24 -0400

    Test de la connexion à la base de données

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from voiture import Voiture
 
 v1 = Voiture("Toyota", "Corolla", 2020, 20000)
 v1.afficher_voiture()
+
+
+from crud_db import connecter_db
+
+conn = connecter_db()
+print("Connexion réussie")
+conn.close()
