431e4ca6af6dd16e4aae654e775d9e9f74e9837a
souhila <souhila.azzouz@monboreal.ca>
2026-04-12 13:36:23 -0400

    Test de la suppression d'une voiture

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ v1 = Voiture("Toyota", "Corolla", 2020, 20000)
 v1.afficher_voiture()
 
 
-from crud_db import connecter_db
+from crud_db import connecter_db, supprimer_voiture
 
 conn = connecter_db()
 print("Connexion réussie")
@@ -15,3 +15,5 @@ from voiture import Voiture
 
 v1 = Voiture("Honda", "Civic", 2021, 22000)
 ajouter_voiture(v1)
+
+supprimer_voiture(1)
