55aced87c2ad7f84432e7546c29e48cb3e7d7ee8
souhila <souhila.azzouz@monboreal.ca>
2026-04-19 23:52:11 -0400

    Ajouter des voitures

---
+++ main.py
@@ -0,0 +1,12 @@
+from voiture import Voiture
+from crud_db import connecter_db, ajouter_voiture, supprimer_voiture, recuperer_voitures, modifier_voiture
+
+
+conn = connecter_db()
+print("Connexion réussie")
+conn.close()
+v1 = Voiture("Toyota", "Corolla", 2020, 20000)
+v2 = Voiture("Honda", "Civic", 2021, 22000)
+
+ajouter_voiture(v1)
+ajouter_voiture(v2)
